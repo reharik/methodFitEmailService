d56f917f7bcfdc45a60983aaca74443f4e5522e1
Raif Harik <harik.raif@gmail.com>
2023-06-03 12:56:17 -0500

    issue copy pasting from stored proc

--- src/dailyPaymentReport/buildDailyPaymentList.js
+++ src/dailyPaymentReport/buildDailyPaymentList.js
@@ -28,7 +28,8 @@ p.EntityId
 FROM Payment as p INNER JOIN [User] AS t ON p.CreatedById = t.EntityId 
 		INNER JOIN Client as c ON p.ClientId = c.EntityId
 WHERE cast(p.CreatedDate At Time Zone 'UTC' At Time Zone 'Eastern Standard Time' As datetime) between
-		@StartDate AND DATEADD(mi, -1,Convert(DateTime, DATEDIFF(DAY, -1, @EndDate)))
+		Convert(DateTime, DATEDIFF(DAY, 0, GETDATE()))
+		AND DATEADD(mi, -1,Convert(DateTime, DATEDIFF(DAY, -1, GETDATE())))
 ORDER BY c.LastName`;
 
 const buildDailyPaymentList = async () => {
